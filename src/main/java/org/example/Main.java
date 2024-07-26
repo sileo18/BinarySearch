2f7e09c9b197d6b930a3c199dfe79d9afaf4b188
sileo18 <lucastoledo358@gmail.com>
2024-07-25 21:08:31 -0300

    other lesson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,41 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int n = 15;
-        System.out.println(binarySearch(n));
+        //int n = 15;
+        //System.out.println(binarySearchSquare(n));
+
+        int[] lista = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+        int nList = 8;
+
+        System.out.println(binarySearchList(lista, nList));
 
 
     }
 
-    public static int binarySearch( int item) {
+    public static int binarySearchList(int[] lista, int item) {
+        int baixo = 0;
+        int alto = lista.length - 1;
+
+
+        while(baixo <= alto) {
+            int meio = (baixo + alto) / 2;
+
+            int chute = lista[meio];
+
+            if(chute == item) {
+                return meio;
+            }
+            if (chute > item) {
+                alto = meio - 1;
+            }
+            else {
+                baixo = meio + 1;
+            }
+        }
+        return -1;
+    };
+
+    public static int binarySearchSquare( int item) {
        int baixo = 0;
        int alto = item;
 
@@ -36,6 +64,8 @@ public class Main {
        }
     return resultado;
     };
+
+
 
 
 }
